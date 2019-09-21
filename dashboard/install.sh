1a8aa7a1bfcb944424993811f45355b2bf242142
kammous <kimo_ammous@yahoo.fr>
2019-09-21 03:23:14 +0000

    HOWTO deploy k8s dashboard

---
+++ dashboard/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+set -e
+
+kubectl apply -f  https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta4/aio/deploy/recommended.yaml
+kubectl -n kubernetes-dashboard wait --timeout=300s --for=condition=available deploy --all
+kubectl apply -f dashboard-adminuser.yml
+kubectl apply -f admin-role-binding.yml
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}') 
+
+echo "Use token above to login to kubernetes dahsboard\n"
+echo "http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
