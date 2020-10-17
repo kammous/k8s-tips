0ae2154e0c416da5802ed52c9495c07cc84ab1b0
kammous <kimo_ammous@yahoo.fr>
2020-10-17 17:18:21 +0200

    upd

--- metallb/uninstall.sh
+++ metallb/uninstall.sh
@@ -4,9 +4,7 @@ set -euxo pipefail
 
 kubectl delete deploy echo
 kubectl delete svc echo
-kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/main/manifes
-ts/metallb.yaml
-kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/main/manifes
-ts/namespace.yaml
+kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/main/manifests/metallb.yaml
+kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/main/manifests/namespace.yaml
 
 
